bd650f96f627aca10576ac5461977dd7c5fca31d
andrey <naumov.a.n@icloud.com>
2024-04-02 01:19:19 +0700

    lesson_part2

--- lesson.cpp
+++ lesson.cpp
@@ -1,45 +1,100 @@
 ﻿#include <iostream>
 #include <string>
 
-class Dog
+class Animal
 {
 public:
-    Dog(int _age)
-	{
-		if (_age > 30 || _age < 0)
-		{
-			std::cerr << "Incorrect age" << std::endl;
-		}
-		else
-		{
-			age = _age;
-		}
-	}
-
 	void setAge(int _age)
 	{
-		if (_age > 30 || _age < 0)
-		{
-			std::cerr << "Incorrect age" << std::endl;
-		} else
-		{
+		if (_age >= 0 && _age < 30)
 			age = _age;
-		}
+		else
+			std::cerr << "Incorrect age" << std::endl;
 	}
 
-	int getAge() const
+	int getAge()
 	{
 		return age;
 	}
 
-private:
+	void setName(std::string _name)
+	{
+		name = _name;
+	}
+
+	std::string getName()
+	{
+		return name;
+	}
+
+protected:
+	std::string name;
 	int age;
 };
 
+class Dog: virtual public Animal
+{
+public:
+Dog() {};
+Dog(int _age, std::string _name)
+{
+	name = _name;
+	if (_age >= 0 && _age < 30)
+		age = _age;
+	else
+		std::cerr << "Incorrect age" << std::endl;
+	}
+
+void barkAge()
+{
+	for (size_t i = 0; i < age; i++)
+		std::cout << "Bark!";
+	std::cout << std::endl;
+}
+private:
+	
+};
+
+class Cat : virtual public Animal
+{
+	public:
+	Cat() {};
+	Cat(int _age, std::string _name)
+	{
+		name = _name;
+		if (_age >= 0 && _age < 30)
+			age = _age;
+		else
+			std::cerr << "Incorrect age" << std::endl;
+	}
+
+	void meowAge()
+	{
+		for (size_t i = 0; i < age; i++)
+			std::cout << "Meow!";
+		std::cout << std::endl;
+	}
+};
+
+class CatDog : public Dog, public Cat
+{
+	public:
+	CatDog(int _age, std::string _name)
+	{
+		name = _name;
+		if (_age >= 0 && _age < 30)
+			age = _age;
+		else
+			std::cerr << "Incorrect age" << std::endl;
+	}
+
+};
+
+
 void lesson()
 {
-	Dog d(10);
-
-	d.setAge(11);
-	std::cout << d.getAge();
+	CatDog d(2, "Balu");
+	d.barkAge();
+	d.setAge(5);
+	d.meowAge();
 }
