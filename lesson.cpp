b74d10389ba7c70b77117d9d26543e109159ae13
andrey <naumov.a.n@icloud.com>
2024-04-02 01:40:57 +0700

    lesson_polymorphism

--- lesson.cpp
+++ lesson.cpp
@@ -4,97 +4,32 @@
 class Animal
 {
 public:
-	void setAge(int _age)
-	{
-		if (_age >= 0 && _age < 30)
-			age = _age;
-		else
-			std::cerr << "Incorrect age" << std::endl;
-	}
-
-	int getAge()
-	{
-		return age;
-	}
-
-	void setName(std::string _name)
-	{
-		name = _name;
-	}
-
-	std::string getName()
-	{
-		return name;
-	}
-
-protected:
-	std::string name;
-	int age;
+	virtual void voice() = 0;
 };
 
-class Dog: virtual public Animal
+class Dog: public Animal
 {
 public:
-Dog() {};
-Dog(int _age, std::string _name)
-{
-	name = _name;
-	if (_age >= 0 && _age < 30)
-		age = _age;
-	else
-		std::cerr << "Incorrect age" << std::endl;
-	}
-
-void barkAge()
-{
-	for (size_t i = 0; i < age; i++)
-		std::cout << "Bark!";
-	std::cout << std::endl;
-}
-private:
-	
-};
-
-class Cat : virtual public Animal
-{
-	public:
-	Cat() {};
-	Cat(int _age, std::string _name)
+	virtual void voice()
 	{
-		name = _name;
-		if (_age >= 0 && _age < 30)
-			age = _age;
-		else
-			std::cerr << "Incorrect age" << std::endl;
-	}
-
-	void meowAge()
-	{
-		for (size_t i = 0; i < age; i++)
-			std::cout << "Meow!";
-		std::cout << std::endl;
+		std::cout << "Гаф!" << std::endl;
 	}
 };
 
-class CatDog : public Dog, public Cat
+class Cat : public Animal
 {
-	public:
-	CatDog(int _age, std::string _name)
+public:
+	virtual void voice()
 	{
-		name = _name;
-		if (_age >= 0 && _age < 30)
-			age = _age;
-		else
-			std::cerr << "Incorrect age" << std::endl;
+		std::cout << "Мяу!" << std::endl;
 	}
-
 };
-
 
 void lesson()
 {
-	CatDog d(2, "Balu");
-	d.barkAge();
-	d.setAge(5);
-	d.meowAge();
+	Animal* c = new Cat();
+	Animal* d = new Dog();
+
+	c->voice();
+	d->voice();
 }
