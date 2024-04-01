ae1444820012f7271949ac37d11eb0b630bc9e4b
andrey <naumov.a.n@icloud.com>
2024-04-02 00:23:48 +0700

    Добавьте файлы проекта.

---
+++ Paradigms.cpp
@@ -0,0 +1,18 @@
+﻿// Paradigms.cpp: определяет точку входа для приложения.
+//
+
+#include "Paradigms.h"
+#include <clocale>
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+void lesson();
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	lesson();
+	return 0;
+}
---
+++ lesson.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+class Dog
+{
+public:
+    Dog(int _age)
+	{
+		if (_age > 30 || _age < 0)
+		{
+			std::cerr << "Incorrect age" << std::endl;
+		}
+		else
+		{
+			age = _age;
+		}
+	}
+
+	void setAge(int _age)
+	{
+		if (_age > 30 || _age < 0)
+		{
+			std::cerr << "Incorrect age" << std::endl;
+		} else
+		{
+			age = _age;
+		}
+	}
+
+	int getAge() const
+	{
+		return age;
+	}
+
+private:
+	int age;
+};
+
+void lesson()
+{
+	Dog d(10);
+
+	d.setAge(11);
+	std::cout << d.getAge();
+}
